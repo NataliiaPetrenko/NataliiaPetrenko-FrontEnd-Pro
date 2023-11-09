8aeb5f75906f6049ebb14429116c7eb1a6dc5a72
Nataliia Petrenko <58181059+NataliiaPetrenko@users.noreply.github.com>
2023-11-09 17:02:34 +0200

    Add files via upload

---
+++ hw 2/app/index.js
@@ -0,0 +1,7 @@
+let firstNumber = prompt("Kindly specify the first number:", "15");
+let secondNumber = prompt("Kindly specify the second number:", "15");
+let thirdNumber = prompt("Kindly specify the third number:", "15");
+
+let averageNumber = (Number(firstNumber) + Number(secondNumber) + Number (thirdNumber))/3
+
+alert (`The average value is ${averageNumber}`);
